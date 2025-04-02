da93f67a18916b889555601b527028060f656e83
bnethanel <bnethanel@gmail.com>
2025-04-02 13:51:57 +0300

    add image folder and start with service and controller logic, add css

---
+++ js/controllers/memeController.js
@@ -0,0 +1,57 @@
+'use strict'
+
+let gElCanvas
+let gCtx
+let gLastPos
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+    resizeCanvas()
+    renderCanvas()
+    renderMeme()
+
+}
+
+function renderMeme() {
+    const meme = getMeme()
+    const imgData = getImgById(meme.selectedImgId)
+    const img = new Image()
+    img.src = imgData.url
+
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+
+        meme.lines.forEach((line, idx) => {
+            gCtx.font = `${line.size}px Impact`
+            gCtx.fillStyle = line.color
+            gCtx.textAlign = 'center'
+            gCtx.fillText(line.txt, gElCanvas.width / 2, 50 + idx * 50)
+        })
+    }
+}
+
+function renderCanvas() {
+
+    //* Set the background default color
+    gCtx.fillStyle = 'white'
+    gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
+
+}
+
+function onClearCanvas() {
+    gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height)
+    renderCanvas()
+}
+
+function renderImg(img) {
+    gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container')
+    gElCanvas.width = elContainer.offsetWidth
+    gElCanvas.height = elContainer.offsetHeight
+    renderCanvas()
+}
---
+++ js/services/memeService.js
@@ -0,0 +1,21 @@
+var gImgs = [{id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat']}]
+var gMeme = {
+ selectedImgId: 1,
+ selectedLineIdx: 0,
+ lines: [
+ {
+ txt: 'I sometimes eat Falafel',
+ size: 20,
+ color: 'red'
+ }
+ ]
+}
+var gKeywordSearchCountMap = {'funny': 12,'cat': 16, 'baby': 2}
+
+function getMeme() {
+    return gMeme
+}
+
+function getImgById(imgId) {
+    return gImgs.find(img => img.id === imgId)
+}
